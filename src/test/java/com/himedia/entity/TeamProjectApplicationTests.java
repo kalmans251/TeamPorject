0cceb525e89c34881710123143bd3036ebe51791
kalmans251 <kalmans251@gmail.com>
2023-03-20 14:16:42 +0900

    팀프로젝트

---
+++ src/test/java/com/himedia/entity/TeamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himedia.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
