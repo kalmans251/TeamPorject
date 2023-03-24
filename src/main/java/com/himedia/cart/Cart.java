d559d16b805c31240638cf9af4706384f3216b1c
jisu555 <superformance9@gmail.com>
2023-03-24 22:12:01 +0900

    Cart 엔티티 작성

---
+++ src/main/java/com/himedia/cart/Cart.java
@@ -0,0 +1,57 @@
+package com.himedia.cart;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.himedia.cartitem.CartItem;
+import com.himedia.member.entity.Member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrePersist;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Cart {
+	
+	@Id
+	@Column(name="cart_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="member_id")
+	private Member member;
+	
+	@DateTimeFormat(pattern = "yyyy-mm-dd")
+	private LocalDate createDate;
+	
+	@PrePersist
+	public void createDate() {
+		this.createDate = LocalDate.now();
+	}
+	
+	@OneToMany(mappedBy = "cart")
+	private List<CartItem> cartItems = new ArrayList<>();
+	
+	public static Cart createCart(Member member) {
+		Cart cart = new Cart();
+		cart.setMember(member);
+		return cart;
+	}
+	
+
+}
