e3f2e529134e73053278e98f6c7ade4b61e2a886
Hayeon Lee <formahappy@gmail.com>
2023-03-27 12:01:56 +0900

    0326

--- src/main/java/com/himedia/item/itemMain/ItemMainRepository.java
+++ src/main/java/com/himedia/item/itemMain/ItemMainRepository.java
@@ -1,9 +1,31 @@
 package com.himedia.item.itemMain;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.himedia.item.entity.Item;
 import com.himedia.item.entity.ItemImg;
+import com.himedia.item.entity.enums.CategoryEnum1;
+import com.himedia.item.entity.enums.CategoryEnum2;
 
-public interface ItemMainRepository extends JpaRepository<ItemImg, Integer> {
-
+public interface ItemMainRepository extends JpaRepository<Item, Integer> {
+ 
+	//카테고리별 분류
+	Page<Item> findByCategoryEnumOrCategoryEnum2(CategoryEnum1 categoryEnum1,CategoryEnum2 categoryEnum2,Pageable pageable);
+	
+	//등록일 순 
+	List<Item> findAllByOrderByRegDateDesc();
+	
+	//가격 높은 순 
+	List<Item> findAllByOrderByPriceAsc();
+	
+	//가격 낮은 순 
+	List<Item> findAllByOrderByPriceDesc();
+	
+	
+	
+	
 }
