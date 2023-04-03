d803a085a1bc1438bbd74573318361c63a5473b1
김동준 <>
2023-04-03 20:24:23 +0900

    no message

---
+++ src/main/java/com/himedia/item/itemMain/ItemOutputListAjaxDto.java
@@ -0,0 +1,28 @@
+package com.himedia.item.itemMain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ItemOutputListAjaxDto {
+	
+	private Long itemId;
+	
+	private String url;
+	
+	private String subject;
+	
+	private Long price;
+	
+	private Integer favorListNum;
+	
+	public ItemOutputListAjaxDto(Long itemId,String url,String subject , Long price,Integer favorListNum) {
+		this.itemId=itemId;
+		this.url=url;
+		this.subject=subject;
+		this.price=price;
+		this.favorListNum=favorListNum;
+	}
+	
+}
