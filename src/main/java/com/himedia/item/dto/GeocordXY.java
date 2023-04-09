03e9f5c91fc0c6f924105062ee1f818fdca20c1a
Kalmans251 <kalmans251@gmail.com>
2023-04-09 12:40:38 +0900

    지역검색 날씨 기반 의류추출 구축

---
+++ src/main/java/com/himedia/item/dto/GeocordXY.java
@@ -0,0 +1,14 @@
+package com.himedia.item.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GeocordXY {
+	
+	private String x;
+	private String y;
+	
+	
+}
---
+++ src/main/java/com/himedia/item/itemMain/ItemListingAjaxDtoTemp.java
@@ -0,0 +1,19 @@
+package com.himedia.item.itemMain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ItemListingAjaxDtoTemp {
+
+	private String sort;
+	
+	private String category;
+	
+	private Integer page;
+	
+	private String search;
+	
+	private Long temp;
+}
